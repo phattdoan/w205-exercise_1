dd3fd0f863e9d88ad8471b12da5c344fb01be65a
Phat Doan <phat.t.doan@gmail.com>
2017-10-28 14:09:47 -0600

    typos in CLEAN.sh

--- loading_and_modelling/CLEAN_load_data_lake.sh
+++ loading_and_modelling/CLEAN_load_data_lake.sh
@@ -5,26 +5,26 @@ MY_CWD=$(pwd)
 
 # remove staging directory
 rm ~/staging/exercise_1/*
-rm ~/staging/exercise_1
-rm ~/staging
+rmdir ~/staging/exercise_1
+rmdir ~/staging
 
 
 # remove hdfs directory
 hdfs dfs -rm /user/w205/hospital_compare/hospital/hospitals.csv
-hdfs dfs -rm /user/w205/hospital_compare/hospital
+hdfs dfs -rmdir /user/w205/hospital_compare/hospital
 hdfs dfs -rm /user/w205/hospital_compare/complications/complications.csv
-hdfs dfs -rm /user/w205/hospital_compare/complications
+hdfs dfs -rmdir /user/w205/hospital_compare/complications
 hdfs dfs -rm /user/w205/hospital_compare/hais/hais.csv
-hdfs dfs -rm /user/w205/hospital_compare/hais
+hdfs dfs -rmdir /user/w205/hospital_compare/hais
 hdfs dfs -rm /user/w205/hospital_compare/returns/returns.csv
-hdfs dfs -rm /user/w205/hospital_compare/returns
+hdfs dfs -rmdir /user/w205/hospital_compare/returns
 hdfs dfs -rm /user/w205/hospital_compare/care/care.csv
-hdfs dfs -rm /user/w205/hospital_compare/care
+hdfs dfs -rmdir /user/w205/hospital_compare/care
 hdfs dfs -rm /user/w205/hospital_compare/measures/measures.csv
-hdfs dfs -rm /user/w205/hospital_compare/measures
+hdfs dfs -rmdir /user/w205/hospital_compare/measures
 hdfs dfs -rm /user/w205/hospital_compare/surveys/surveys.csv
-hdfs dfs -rm /user/w205/hospital_compare/surveys
-hdfs dfs -rm /user/w205/hospital_compare
+hdfs dfs -rmdir /user/w205/hospital_compare/surveys
+hdfs dfs -rmdir /user/w205/hospital_compare
 
 # change directory back to the original
 cd $MY_CWD
