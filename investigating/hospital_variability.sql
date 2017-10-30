6272b13b7c8ece239a43c447b1afcd74c286a9bf
phattdoan <phat.t.doan@gmail.com>
2017-10-30 13:59:07 -0600

    changes to variability script

--- investigating/hospital_variability.sql
+++ investigating/hospital_variability.sql
@@ -1,11 +1,12 @@
-drop table hospital_variability;
-create table hospital_variability as
+drop table procedure_variability;
+create table procedure_variability as
 select measure_id, variance(score) var_id, count(*) count
 from care_transformed
 group by measure_id
 having count > 30;
 select h.measure_id,m.measure_name,h.var_id 
-from hospital_variability h, transform_measures m
-where h.measure_id = m.measure_id
+from procedure_variability pv
+join measures m
+on h.measure_id = m.measure_id
 order by var_id desc
 limit 10;
