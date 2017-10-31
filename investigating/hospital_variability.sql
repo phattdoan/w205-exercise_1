862690ed164bb6a14daf067da08ede33357fe92a
Phat Doan <phat.t.doan@gmail.com>
2017-10-30 23:33:03 -0600

    added q4 scripts

--- investigating/hospital_variability.sql
+++ investigating/hospital_variability.sql
@@ -5,8 +5,7 @@ from care_transformed
 group by measure_id
 having count > 30;
 select m.measure_id, m.measure_name, pv.var_id 
-from procedure_variability pv
-join measures m
-on m.measure_id = m.measure_id
+from procedure_variability pv, measures m
+where m.measure_id = m.measure_id
 order by pv.var_id desc
-limit 10;
+limit 20;
