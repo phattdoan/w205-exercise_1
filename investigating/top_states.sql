819e5807c01ea93ff7d6fe08e2aef6f194a38f7a
Phat Doan <phat.t.doan@gmail.com>
2017-10-30 23:02:25 -0600

    fixed typos on top_states

--- investigating/top_states.sql
+++ investigating/top_states.sql
@@ -1,12 +1,16 @@
 -- top states
-select top_50_hospitals.state, cast(count(*)/hospital_count as float) as perct_top_50_hospitals
-from (select i.hospital_name, i.city, i.state, i.hospital_ownership,
+drop table top_50_hospitals;
+create table top_50_hospitals as
+select i.hospital_name, i.city, i.state, i.hospital_ownership,
 	top.total_score 
 	from top_hospitals top
 	left join hospital_info i
 		on i.provider_id = top.provider_id
 	order by total_score desc
-	limit 50) top_50_hospitals
+	limit 50;
+select th50.state, cast(count(*)/first(c.hospital_count) as float) as perct_top_50_hospitals
+from top_50_hospitals th_50
 left join states_hospital_count c
-	top_50_hospitals.state = c.state
+	on th50.state = c.state
+group by th50.state
 order by perct_top_50_hospitals desc;
